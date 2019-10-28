0417d1eff2b24bba5e0fcf582471345fd0d54225
alce65 <alce65@hotmail.es>
2019-10-28 18:50:33 +0100

    Ejemplo de jasmine

--- src/app/tareas/tareas-plus/lista-tareas/lista-tareas.component.spec.ts
+++ src/app/tareas/tareas-plus/lista-tareas/lista-tareas.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListaTareasComponent } from './lista-tareas.component';
+import { ShareModule } from 'src/app/share/share.module';
+import { NewTareaComponent } from '../new-tarea/new-tarea.component';
+import { TareaComponent } from '../tarea/tarea.component';
+import { FormsModule } from '@angular/forms';
 
-describe('ListaTareasComponent', () => {
+fdescribe('ListaTareasComponent', () => {
   let component: ListaTareasComponent;
   let fixture: ComponentFixture<ListaTareasComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListaTareasComponent ]
+      declarations: [ ListaTareasComponent, NewTareaComponent, TareaComponent ],
+      imports: [ ShareModule, FormsModule]
     })
     .compileComponents();
   }));
